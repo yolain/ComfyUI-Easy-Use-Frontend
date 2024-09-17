8712f2615f180ba823ad1349613b5cfa7b9988a1
yolain <me@yolain.com>
2024-09-17 16:28:38 +0800

    Support subgraph time taken accumulate

--- src/extensions/timeTaken.js
+++ src/extensions/timeTaken.js
@@ -9,20 +9,29 @@ app.registerExtension({
     name: 'Comfy.EasyUse.TimeTaken',
 
     setup() {
-        const timeMap =  new Map();
+        let timeMap =  new Map();
         let lastId = 0;
+        api.addEventListener('execution_start', data =>{
+            if(graph){
+                graph._nodes.forEach(node => {
+                    if(node.executionDuration) delete node.executionDuration;
+                })
+            }
+        });
         api.addEventListener('executing', data => {
             const enabled = getSetting('EasyUse.Nodes.Runtime',null, true);
             if(!enabled) return;
             const id = data?.node || data?.detail || null;
-            const node = getNodeById(id);
-            if (node) node.executionDuration = ``;
             const timeTaken = timeMap.get(lastId);
             timeMap.delete(lastId);
             if (lastId && timeTaken) {
                 const delta = Date.now() - timeTaken;
                 const lastNode = getNodeById(lastId);
-                if (lastNode) lastNode.executionDuration = `${(delta / 1e3).toFixed(2)}${$t('s')}`
+                if (lastNode) {
+                    if(!lastNode.executionDuration) lastNode.executionDuration = 0.00;
+                    console.log(lastId, id)
+                    lastNode.executionDuration = lastNode.executionDuration + (delta / 1e3)
+                }
             }
             lastId = id;
             timeMap.set(id, Date.now());
@@ -34,7 +43,7 @@ app.registerExtension({
         const orig = nodeType.prototype.onDrawForeground;
         nodeType.prototype.onDrawForeground = function(...args) {
           const [ctx] = args;
-          drawTime(ctx, this.executionDuration || ``);
+          drawTime(ctx, this.executionDuration);
           return orig == null ? undefined : orig.apply(this, args);
         };
     }
@@ -48,6 +57,7 @@ app.registerExtension({
  */
 function drawTime(ctx, text) {
     if(!text) return
+    text = text.toFixed(3) +$t('s')
     ctx.save();
     ctx.fillStyle = LiteGraph.NODE_DEFAULT_BGCOLOR
     drawRoundedRect(ctx, 0, - LiteGraph.NODE_TITLE_HEIGHT - 20, ctx.measureText(text).width + 10, LiteGraph.NODE_TITLE_HEIGHT - 10, 4);
