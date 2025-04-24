f27d7e9ee54dd237a324b77420f28aa08f54f92d
yolain <yolain@163.com>
2025-04-24 14:35:06 +0800

    Fix hidden item judgment needs to be delayed when first loading the page

--- src/extensions/widget.js
+++ src/extensions/widget.js
@@ -741,10 +741,14 @@ function toggleLogic(node, widget) {
             if (v === "自定义 x 自定义") {
                 widget.value = 'width x height (custom)'
             }
-            ['empty_latent_width', 'empty_latent_height', 'width', 'height'].map(name => toggleWidget(node, getWidgetByName(node, name), v === 'width x height (custom)' ? true : false))
+            setTimeout(_ => {
+                ['empty_latent_width', 'empty_latent_height', 'width', 'height'].map(name => toggleWidget(node, getWidgetByName(node, name), v === 'width x height (custom)' ? true : false))
+            },1)
             break
         case 'ratio':
-            ['empty_latent_width', 'empty_latent_height'].map(name => toggleWidget(node, getWidgetByName(node, name), v === 'custom' ? true : false))
+            setTimeout(_ => {
+                ['empty_latent_width', 'empty_latent_height'].map(name => toggleWidget(node, getWidgetByName(node, name), v === 'custom' ? true : false))
+            },1)
             break
         case 'num_loras':
             var number_to_show = v + 1
